70307e443224e03f851f4664a75ed828f561d9ed
Leonardo Benitez <>
2025-05-29 13:07:51 -0500

    passing data to a view

--- src/main/java/com/leo/spring/app1/app1/controllers/EjemploController.java
+++ src/main/java/com/leo/spring/app1/app1/controllers/EjemploController.java
@@ -1,6 +1,9 @@
 package com.leo.spring.app1.app1.controllers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -8,7 +11,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class EjemploController {
 
     @GetMapping("/detail")
-    public String info() {
+    /* public String info(Model model) {
+        model.addAttribute("Title", "Detail Product");
+        model.addAttribute("Product", "Product 1"); 
+        model.addAttribute("Description", "description of product 1");
+
+        return "detail_product";
+    }*/
+
+    public String info(Map<String, Object> model) {
+        model.put("Title", "Detail Product");
+        model.put("Product", "Product 1");
+        model.put("Description", "description of product 1");
 
         return "detail_product";
     }
